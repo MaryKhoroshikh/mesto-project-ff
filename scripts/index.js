6b45959a7118f66471d1ec5c5c45cece98acdb3d
Mary Good <goodmary7@yandex.ru>
2024-10-29 20:51:52 +0300

    1.0.0. add inicial cards

--- scripts/index.js
+++ scripts/index.js
@@ -11,30 +11,41 @@
 // 
 const container = document.querySelector('.content');
 const placesSection = container.querySelector('.places');
-const places = placesSection.querySelector('.places__list');
+const placesContainer = placesSection.querySelector('.places__list');
 
 // функция создания
-function addCard(imageValue, titleValue, deleteCard) {
+function createCard(Value, deleteCard) {
     // функция удаления
     function deleteCard (evn) {
         evn.target.parentElement.remove();
     }
+    // параметры
+    const imageValue = Value[0];
+    const titleValue = Value[1];
     // темплейт
     const placeTemplate = document.querySelector('#card-template').content;
     // клонируем
     const placeElement = placeTemplate.querySelector('.places__item').cloneNode(true);
     // наполняем
-    placeElement.querySelector('.card__image').src = imageValue;
+    const cardImage = placeElement.querySelector('.card__image');
+    cardImage.src = imageValue;
+    cardImage.alt = titleValue;
     placeElement.querySelector('.card__title').textContent = titleValue;
     // вешаем слушатель на кнопку удалить
     placeElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
-    // добавляем карточку
-    places.append(placeElement);
+    // возвращаем карточку
+    return placeElement;
+}
+
+// функция удаления
+function deleteCard (evn) {
+    evn.target.parentElement.remove();
 }
 
 //вывод карточек
 initialCards.forEach(function (elem) {
-    let link = elem.link;
-    let name = elem.name;
-    addCard(link, name);
+    const link = elem.link;
+    const name = elem.name;
+    const values = [{link}, {name}];
+    placesContainer.append(createCard(values));
 });
